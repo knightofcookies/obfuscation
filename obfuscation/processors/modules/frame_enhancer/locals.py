c98a06e48448643c3321b485231204b035b16576
knightofcookies <101050984+knightofcookies@users.noreply.github.com>
2025-11-12 03:42:34 +0530

    First commit

---
+++ obfuscation/processors/modules/frame_enhancer/locals.py
@@ -0,0 +1,18 @@
+from obfuscation.types import Locals
+
+LOCALS : Locals =\
+{
+	'en':
+	{
+		'help':
+		{
+			'model': 'choose the model responsible for enhancing the frame',
+			'blend': 'blend the enhanced into the previous frame'
+		},
+		'uis':
+		{
+			'blend_slider': 'FRAME ENHANCER BLEND',
+			'model_dropdown': 'FRAME ENHANCER MODEL'
+		}
+	}
+}
